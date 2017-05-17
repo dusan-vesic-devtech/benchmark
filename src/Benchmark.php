ff69c520bdf3a83aa00c0482980a6e6b45c32092
vesic <dusan.vesic@gmail.com>
2017-05-17 13:19:40 +0000

    Added memory usage functinality

--- src/Benchmark.php
+++ src/Benchmark.php
@@ -5,8 +5,8 @@ namespace Vesic\Benchmark;
 class Benchmark {
     
     protected $_start;
-    
     protected $_end;
+    protected $_memoryUsage;
     
     public function startTime() {
         $this->_start = microtime(true);
@@ -14,13 +14,19 @@ class Benchmark {
     
     public function endTime() {
         $this->_end = microtime(true);
+        $this->_memoryUsage = memory_get_usage();
     }
     
     public function run($fn) {
         $this->startTime();
         call_user_func_array($fn, []);
         $this->endTime();
-        echo round(($this->end - $this->start), 3);
+        echo 'time: ' . round(($this->_end - $this->_start), 3) . '. Memory: ' . $this->convertToHumanReadable($this->_memoryUsage);
+    }
+    
+    public function convertToHumanReadable($size) {
+        $unit = array('b','kb','mb','gb','tb','pb');
+        return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
     }
     
 }
