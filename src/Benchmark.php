d60b05a82d72fd72ac41446206f0dc80a0cb2e5e
vesic <dusan.vesic@gmail.com>
2017-05-17 12:40:20 +0000

    Added essential functionality.

--- src/Benchmark.php
+++ src/Benchmark.php
@@ -4,8 +4,23 @@ namespace Vesic\Benchmark;
 
 class Benchmark {
     
-    public function run() {
-        
+    protected $start;
+    
+    protected $end;
+    
+    public function start() {
+        $this->start = microtime(true);
+    }
+    
+    public function end() {
+        $this->end = microtime(true);
+    }
+    
+    public function run($fn) {
+        $this->start();
+        call_user_func_array($fn, []);
+        $this->end();
+        echo round(($this->end - $this->start), 3);
     }
     
 }
